a7e0694d6545aa840a56a4e21b647c8e7603f70f
root <sujanbarman12345q@gmail.com>
2020-08-02 15:16:58 +0000

    first

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, ScrollView } from 'react-native-gesture-handler';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    Hello : "Hey",
+    hmm : "ok"
+  }
+  changek = () => this.setState({
+    Hello:"changed"
+  })
+  render(){
+    return (
+        <View onTouchMove={() => this.setState({Hello:"ok"})} style={styles.container}>
+          <Text>{this.state.Hello}</Text>
+          <TextInput defaultValue="Type text"/>
+          <StatusBar style="auto" />
+        </View>
+        
+      
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#abc',
     alignItems: 'center',
     justifyContent: 'center',
   },
